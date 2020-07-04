d26a1096c632f5025cca6cf006b661bc459d4d1a
john <lrc272317227@gmail.com>
2020-07-04 11:14:47 +0800

    添加INIT

---
+++ src/main/scala/com/alibaba/wordcount/WordCount.scala
@@ -0,0 +1,24 @@
+package com.alibaba.wordcount
+
+import org.apache.flink.api.scala._
+
+/**
+  * 批处理
+  */
+object WordCount {
+  def main(args: Array[String]): Unit = {
+    //创建一个批处理的执行环境
+    val env: ExecutionEnvironment = ExecutionEnvironment.getExecutionEnvironment;
+    //从文件中读取数据
+    val inputDataSet: DataSet[String] = env.readTextFile("/Users/john/Desktop/学习/tutorial/src/main/resources/word.tx");
+    //基于 dataSet 做转换，首先按照空格分词，然后按照 word 作为 key 做 group by
+    val resultDataSet: AggregateDataSet[(String, Int)] = inputDataSet.
+      flatMap(_.split(" ")) //分词得到所有 word 构成的数据集
+      .map((_, 1)) //转换成一个二元组 （word， count）
+      .groupBy(0) //以二元组中第一个元素作为 Key 分组
+      .sum(1) //聚合二元组中第二个元素的值
+
+    //打印输出
+    resultDataSet.print()
+  }
+}
