c2b78bdba8cb7786bf502f4e44ef0d61090fe48a
john <lrc272317227@gmail.com>
2020-07-28 23:44:54 +0800

    SQL

--- src/main/scala/SQL/TableTest.scala
+++ src/main/scala/SQL/TableTest.scala
@@ -1,9 +1,9 @@
 package SQL
 
 import org.apache.flink.streaming.api.scala._
-import org.apache.flink.table.api.Table
+import org.apache.flink.table.api.{DataTypes, Table}
 import org.apache.flink.table.api.scala._
-import streamApi.SensorReading
+import org.apache.flink.table.descriptors.{FileSystem, OldCsv, Schema}
 
 
 /**
@@ -15,28 +15,45 @@ object TableTest {
     env.setParallelism(1)
     //基于 env 创建表环境
     val tableEnv: StreamTableEnvironment = StreamTableEnvironment.create(env)
-    //创建表执行环境
-    val inputStream: DataStream[String] = env.readTextFile("/Users/john/Desktop/学习/tutorial/src/main/resources/sensor.tx")
+    val filePath = "/Users/john/Desktop/学习/tutorial/src/main/resources/sensor.tx"
 
-    //map 成样例类类型
-    val dataStream : DataStream[SensorReading] = inputStream.map(data =>{
-      val dataArray = data.split(",")
-      SensorReading(dataArray(0), dataArray(1).toLong, dataArray(2).toDouble)
-    })
+    //定义初始表的结构
+    tableEnv.connect(new FileSystem().path(filePath))
+      .withFormat(new OldCsv())
+      .withSchema(new Schema()
+        .field("id", DataTypes.STRING())
+          .field("timestamp", DataTypes.BIGINT())
+          .field("temperature", DataTypes.DOUBLE())
+      ).createTemporaryTable("inputTable")
 
-    //基于 tableEnv, 将流转换成表
-    val dataTable:Table = tableEnv.fromDataStream(dataStream)
-
-    //调用 table api 做转换操作
-    val resultTable : Table = dataTable
-      .select("id, temperature")
-      .filter("id == 'sensor_1'")
-
-    //把表转换成流，打印输出
-    val resultStream: DataStream[(String, Double)] = resultTable
-      .toAppendStream[(String, Double)]
-
-    resultStream.print()
-    env.execute("table api")
+    val inputTable: Table = tableEnv.from("inputTable")
+    inputTable.toAppendStream[(String, Long, Double)].print()
+    env.execute("table api test")
+//
+//    //基于 env 创建表环境
+//    val tableEnv: StreamTableEnvironment = StreamTableEnvironment.create(env)
+//    //创建表执行环境
+//    val inputStream: DataStream[String] = env.readTextFile("/Users/john/Desktop/学习/tutorial/src/main/resources/sensor.tx")
+//
+//    //map 成样例类类型
+//    val dataStream : DataStream[SensorReading] = inputStream.map(data =>{
+//      val dataArray = data.split(",")
+//      SensorReading(dataArray(0), dataArray(1).toLong, dataArray(2).toDouble)
+//    })
+//
+//    //基于 tableEnv, 将流转换成表
+//    val dataTable:Table = tableEnv.fromDataStream(dataStream)
+//
+//    //调用 table api 做转换操作
+//    val resultTable : Table = dataTable
+//      .select("id, temperature")
+//      .filter("id == 'sensor_1'")
+//
+//    //把表转换成流，打印输出
+//    val resultStream: DataStream[(String, Double)] = resultTable
+//      .toAppendStream[(String, Double)]
+//
+//    resultStream.print()
+//    env.execute("table api")
   }
 }
